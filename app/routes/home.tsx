73d4de6320411944ce504e2f031fe924ab50d730
Sandip <sandipghosh0312@gmail.com>
2026-02-17 20:14:45 +0530

    Implement homepage ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import Navbar from "components/Navbar";
 import type { Route } from "./+types/home";
+import Button from "components/ui/Button";
+import { ArrowUpRight, Clock, Layers } from "lucide-react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,6 +14,67 @@ export default function Home() {
   return (
     <div className="home">
       <Navbar />
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"/>
+          </div>
+          <p>Introducing the brand new ArchitexAI</p>
+        </div>
+        <h1>Build beautiful spaces at the speed of thought with ArchitexAI</h1>
+        <p className="subtitle">ArchitexAI is an AI-first design environment that helps you visualize, render and ship architectual projects faster than ever.</p>
+        <div className="actions">
+            <a href="#upload" className="cta">Start Building</a>
+            <Button size="lg" variant="outline" className="demo">Watch Demo</Button>
+        </div>
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG, formats upto 10MB.</p>
+            </div>
+            <p>Upload Images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>Your latest work and shared community projects, all in one place.</p>
+            </div>
+          </div>
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="Project" />
+                <div className="badge">
+                  <span>Community Projects</span>
+                </div>
+              </div>
+              <div className="card-body">
+                <div>
+                  <h3>Project Manhattan</h3>
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date('01.01.2027').toLocaleDateString()}</span>
+                    <span>By Manhattan City Projects</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
     
